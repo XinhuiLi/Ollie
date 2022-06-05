a58449095d4672f35ab901c870f807bb792352bd
Yannan Chen <yc3338@columbia.edu>
2022-06-05 00:01:19 -0400

    Add missing dependencies

---
+++ v0.0.2/SomeOtherDependencies/function and GUI/WheelCallback.m
@@ -0,0 +1,15 @@
+function [handles,hObject] = WheelCallback(Tag,handles,hObject)
+
+% save('obj.mat','handles')
+
+% flds = {'Normal','Abnormal'};
+% Callback = {'WheelCallback'};
+% % pos = [.25,0.015,.24,.015];
+% pos = [.65,0.015 ,0.16,0.211];
+% 
+% s = funBuildo('wheel','spinner',pos,flds,...
+%     [handles.score(handles.currentID),1-handles.score(handles.currentID)],'Callback',Callback);
+% handles = funControl('wheel',handles,s);
+
+end
+
--- v0.0.2/run_Ollie.m
+++ v0.0.2/run_Ollie.m
@@ -1,5 +1,6 @@
 % Example_Program holds all the core code..
 
 clear, clc;close all; warning off;
+addpath('./SomeOtherDependencies')
 close(Demo)
 Demo
